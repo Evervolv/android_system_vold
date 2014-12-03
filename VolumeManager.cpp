ec9d7f5ce6ba5ececfbe98b6049f1e0c7ceec2a1
Tom Marshall <tdm@cyngn.com>
2014-12-02 16:48:08 -0800

    vold: Support unmount <path> detach
    This is to be used primarily (exclusively?) in recovery.  In prior
    versions, the local file was copied to tmpfs so that everything could
    be unmounted cleanly.  But the current version uses the local file
    in-place due to constraints with low memory devices.  This prevents
    the filesystem that holds the local file from being unmounted in the
    normal way.  So in order to avoid leaking a mount, the filesystem that
    holds the local file must be unmounted with the detach flag.
    
    Change-Id: I17716f131225b54ca3bc9d2215a45d22d1841137

--- VolumeManager.cpp
+++ VolumeManager.cpp
@@ -1755,7 +1755,7 @@ int VolumeManager::getDirectVolumeList(struct volume_info *vol_list) {
     return 0;
 }
 
-int VolumeManager::unmountVolume(const char *label, bool force, bool revert) {
+int VolumeManager::unmountVolume(const char *label, bool force, bool revert, bool detach) {
     Volume *v = lookupVolume(label);
 
     if (!v) {
@@ -1777,7 +1777,7 @@ int VolumeManager::unmountVolume(const char *label, bool force, bool revert) {
 
     cleanupAsec(v, force);
 
-    return v->unmountVol(force, revert);
+    return v->unmountVol(force, revert, detach);
 }
 
 extern "C" int vold_unmountAllAsecs(void) {
