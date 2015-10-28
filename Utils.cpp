d5b7195806e3a0bad639374d579b3ca29b9b8772
Dan Pasanen <dan.pasanen@gmail.com>
2015-10-27 22:52:37 -0500

    vold: add support for more filesystems for public storage
    * Add exfat and ntfs support based off f2fs and ported to use
      fuse
    * Add support for both along with f2fs and ext4 to PublicVolume
    * Also attempt to mount any volume if it's been determined that
      the kernel supports it
    
    Change-Id: I0a83761cefd97791e3ec84a18e199dfd27a5ed0b
    
    vold: fs: Fix build errors
    
    * Migrate from base to android-base
    * Add missing , in Ext4 Mount function
    
    [AdrianDC] Ignore unpatched ext4 arguments
    [mikeioannina] Update for Pie native exfat
    
    Change-Id: I875b5763c472aa7da2976ec7c5db7cf28c913876
    
    vold: ntfs: Use strlcat
    
    Clang now enforces length checking :/
    
    Change-Id: I495b4cb2ee530e72b1084248f0549d63589523b0
    
    Change-Id: I0a83761cefd97791e3ec84a18e199dfd27a5ed0b

--- Utils.cpp
+++ Utils.cpp
@@ -1055,6 +1055,10 @@ bool IsFilesystemSupported(const std::string& fsType) {
         PLOG(ERROR) << "Failed to read supported filesystems";
         return false;
     }
+
+    /* fuse filesystems */
+    supported.append("fuse\tntfs\n");
+
     return supported.find(fsType + "\n") != std::string::npos;
 }
 
