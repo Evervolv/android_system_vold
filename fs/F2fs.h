bf5b7ff735e1917af8f6d0976a74d05cf1d992ae
Tom Marshall <tdm@cyngn.com>
2015-11-05 11:20:54 -0800

    vold: Honor mount options for ext4/f2fs partitions
     * Save mount options and fstype for DiskPartition objects
     * Pass mount options and fstype to PublicVolume ctor
     * Pass mount options to ext4::Mount/f2fs:Mount
     * Use specified/default fstype if blkid fails
    
    This is trivially extensible for other fs types.
    
    [mikeioannina] Adapt for Pie and Q
    
    Change-Id: Ie5dd27f26a4a7129dd9efe6a7ad0a589928282a0

--- fs/F2fs.h
+++ fs/F2fs.h
@@ -28,7 +28,7 @@ namespace f2fs {
 bool IsSupported();
 
 status_t Check(const std::string& source, bool trusted);
-status_t Mount(const std::string& source, const std::string& target);
+status_t Mount(const std::string& source, const std::string& target, const std::string& opts = "");
 status_t Format(const std::string& source);
 
 }  // namespace f2fs
