316548013d4beb7f53effdda57c4051776aed667
Tom Marshall <tdm@cyngn.com>
2015-11-05 11:20:54 -0800

    vold: Honor mount options for ext4/f2fs partitions
     * Save mount options and fstype for DiskPartition objects
     * Pass mount options and fstype to PublicVolume ctor
     * Pass mount options to ext4::Mount/f2fs:Mount
     * Use specified/default fstype if blkid fails
    
    This is trivially extensible for other fs types.
    
    [mikeioannina] Adapt for Pie and Q
    
    Change-Id: Ie5dd27f26a4a7129dd9efe6a7ad0a589928282a0

--- main.cpp
+++ main.cpp
@@ -276,6 +276,14 @@ static int process_config(VolumeManager* vm, VoldConfigs* configs) {
             }
 
             std::string sysPattern(entry.blk_device);
+            std::string fstype;
+            if (!entry.fs_type.empty()) {
+                fstype = entry.fs_type;
+            }
+            std::string mntopts;
+            if (!entry.fs_options.empty()) {
+                mntopts = entry.fs_options;
+            }
             std::string nickname(entry.label);
             int flags = 0;
 
@@ -289,7 +297,8 @@ static int process_config(VolumeManager* vm, VoldConfigs* configs) {
             }
 
             vm->addDiskSource(std::shared_ptr<VolumeManager::DiskSource>(
-                new VolumeManager::DiskSource(sysPattern, nickname, flags)));
+                new VolumeManager::DiskSource(sysPattern, nickname, flags,
+                        fstype, mntopts)));
         }
     }
     return 0;
