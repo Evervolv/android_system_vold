d3c471f6d4ac0a6d5195ee6a0c295bd96d8b4102
Neeraj Soni <neersoni@codeaurora.org>
2018-12-14 15:18:15 +0530

    system: vold: Use wrapped key for metadata encryption
    Wrapped key feature is needed for better security of encryption keys and to
    ensure data integrity when crypto key cache is cleared during reset operation
    of storage/crypto hardware.
    
    Original patch: https://source.codeaurora.org/quic/la/platform/system/vold/commit/?h=LA.QSSI.11.0.r1-05600-qssi.0&id=c480f913e6abc2757c0d79afba5a3df1c4adc731
    [Pig]: Clean up all deprecated codes that were removed during latter
    merge.
    
    CRs-Fixed: 2367150
    Change-Id: I83d14861bf81e102151fa3417d84008c214a9ac0
    
    vold: Enable legacy support for wrapped key
    
    Legacy wrapped key support was dropped while merging changes
    to support multiple versions of dm-default key driver in kernel.
    Fix this by calling legacy API to check wrapped key support for
    metadata encryption.
    
    CRs-Fixed: 2678344
    Change-Id: I7d9efec09ddf7169cf0b1114b4e16b9fe38cad4b
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>
    
    system: vold: Upgrade the FBE key
    
    During OTA upgrades if security state or ROT changes then Keymaster
    keys requires upgrade. So for such usescases, if the FBE ephemeral
    key export fails, check whether KM key requires upgrade and try for
    exporting ephemeral key again.
    
    CRs-Fixed: 2632902
    Change-Id: I3ee2fcd97a56b628dc4304867c8f2b8da875f883
    Signed-off-by: Neeraj Soni <neersoni@codeaurora.org>
    
    vold: Bring in more wrapped key changes
    
    Change-Id: I44e81afaec78c567a0bf2eed30a79eb737e2a867

--- KeyStorage.cpp
+++ KeyStorage.cpp
@@ -141,8 +141,11 @@ bool generateWrappedStorageKey(KeyBuffer* key) {
     if (!keymaster) return false;
     std::string key_temp;
     auto paramBuilder = km::AuthorizationSetBuilder().AesEncryptionKey(AES_KEY_BYTES * 8);
-    paramBuilder.Authorization(km::TAG_ROLLBACK_RESISTANCE);
-    paramBuilder.Authorization(km::TAG_STORAGE_KEY);
+    km::KeyParameter param1;
+    param1.tag = static_cast<::android::hardware::keymaster::V4_0::Tag>(
+        android::hardware::keymaster::V4_0::KM_TAG_FBE_ICE);
+    param1.f.boolValue = true;
+    paramBuilder.push_back(param1);
     if (!keymaster.generateKey(paramBuilder, &key_temp)) return false;
     *key = KeyBuffer(key_temp.size());
     memcpy(reinterpret_cast<void*>(key->data()), key_temp.c_str(), key->size());
@@ -154,7 +157,20 @@ bool exportWrappedStorageKey(const KeyBuffer& kmKey, KeyBuffer* key) {
     if (!keymaster) return false;
     std::string key_temp;
 
-    if (!keymaster.exportKey(kmKey, &key_temp)) return false;
+    auto ret = keymaster.exportKey(kmKey, &key_temp);
+    if (ret != km::ErrorCode::OK) {
+        if (ret == km::ErrorCode::KEY_REQUIRES_UPGRADE) {
+           std::string kmKeyStr(reinterpret_cast<const char*>(kmKey.data()), kmKey.size());
+           std::string Keystr;
+           if (!keymaster.upgradeKey(kmKeyStr, km::AuthorizationSet(), &Keystr)) return false;
+           KeyBuffer upgradedKey = KeyBuffer(Keystr.size());
+           memcpy(reinterpret_cast<void*>(upgradedKey.data()), Keystr.c_str(), upgradedKey.size());
+           ret = keymaster.exportKey(upgradedKey, &key_temp);
+           if (ret != km::ErrorCode::OK) return false;
+        } else {
+           return false;
+        }
+    }
     *key = KeyBuffer(key_temp.size());
     memcpy(reinterpret_cast<void*>(key->data()), key_temp.c_str(), key->size());
     return true;
